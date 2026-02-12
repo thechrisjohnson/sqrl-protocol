658e8da8a20b111c3c87cf23b878d615e9888855
Chris Johnson <thechrisjohnson@protonmail.com>
2026-02-12 10:47:42 -0500

    Modify tests to match updates to rand crate

--- src/server_response.rs
+++ src/server_response.rs
@@ -241,7 +241,7 @@ impl TIFValue {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rand::{distr::Alphanumeric, rng, Rng};
+    use rand::{RngExt, distr::Alphanumeric, rng};
 
     const TEST_SERVER_RESPONSE: &str = "dmVyPTENCm51dD0xV005bGZGMVNULXoNCnRpZj01DQpxcnk9L2NsaS5zcXJsP251dD0xV005bGZGMVNULXoNCnN1az1CTUZEbTdiUGxzUW9qdUpzb0RUdmxTMU1jbndnU2N2a3RGODR2TGpzY0drDQo";
 
